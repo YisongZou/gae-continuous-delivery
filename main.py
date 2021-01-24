e48d54e75d83d4b5c932fd855e5d7ec5c15250b8
Yisong Zou <yiosng.zou@duke.edu>
2021-01-24 05:06:01 +0000

    Fix2

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def hello():
     print("I am inside hello world")
     return 'Hello World! CD.  This is auto-deploy.  Take 10'
 
-@app.route('/echo/<name>')
+@app.route('/echo1/<name>')
 def echo(name):
     print(f"This was placed in the url: new-{name}")
     val = {"new-name": name}
